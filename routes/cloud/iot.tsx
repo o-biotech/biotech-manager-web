2cb9dca226231becb1db4718d508da97ea2817b5
mcgear <michael.gearhardt@gmail.com>
2023-07-18 10:50:13 -0600

    text fix

--- routes/cloud/iot.tsx
+++ routes/cloud/iot.tsx
@@ -25,14 +25,10 @@ export default function CloudIoT(props: PageProps) {
               IoT infrastructure not configured
             </label>
 
-            <input
-              id="resource-group-name"
-              name="resource-group-name"
-              type="text"
-              required
-              placeholder="Enter new resource group name"
-              class="appearance-none block w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 rounded leading-tight focus:outline-none focus:border-blue-500"
-            />
+            <p>
+              Configure your IoT infrastructure to get started with device data
+              flows and APIs.
+            </p>
           </div>
         </div>
 
