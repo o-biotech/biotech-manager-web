cf42930ce0cbc830348cca015a19b224a37a289f
mcgear <michael.gearhardt@gmail.com>
2023-07-22 19:50:18 -0600

    Responsive forms

--- components/organisms/cloud/CloudStepsFeatures.tsx
+++ components/organisms/cloud/CloudStepsFeatures.tsx
@@ -1,16 +1,9 @@
-import { ComponentChildren } from "preact";
-import { Action, ActionStyleTypes } from "$atomic/atoms/Action.tsx";
-import { ActionGroup } from "$atomic/molecules/ActionGroup.tsx";
-import { DisplayStyleTypes } from "$atomic/molecules/Display.tsx";
-import { Features, FeaturesProps } from "$atomic/organisms/Features.tsx";
-import { classSet } from "$atomic/utils/jsx.tsx";
+import { ComponentChildren, JSX } from "preact";
 import { CloudPhaseTypes } from "../../CloudPhaseTypes.tsx";
 import {
   StepsFeatures,
   StepsFeaturesProps,
 } from "$atomic/organisms/StepsFeatures.tsx";
-import { callToActionStyles } from "../../styles/actions.tsx";
-import CloudConnectManagedForm from "./connect/managed.form.tsx";
 import CloudCALZForm from "./calz.form.tsx";
 import CloudConnectForms from "../../../islands/organisms/CloudConnectForms.tsx";
 import CloudIoTForm from "./iot.form.tsx";
@@ -20,26 +13,49 @@ export interface CloudStepsFeaturesProps extends StepsFeaturesProps {
 }
 
 export default function CloudStepsFeatures(props: CloudStepsFeaturesProps) {
+  let currentForm: ComponentChildren = undefined;
+
+  switch (props.cloudPhase) {
+    case CloudPhaseTypes.Connect:
+      currentForm = <CloudConnectForms class="px-4" />;
+      break;
+
+    case CloudPhaseTypes.CALZ:
+      currentForm = <CloudCALZForm class="px-4" />;
+      break;
+
+    case CloudPhaseTypes.Infrastucture:
+      currentForm = <CloudIoTForm class="px-4" />;
+      break;
+  }
+
+  const smCurrentForm = <div class="flex md:hidden">{{ ...currentForm }}</div>;
+
+  const mdCurrentForm = (
+    <div class="hidden md:(flex justify-center)">{{ ...currentForm }}</div>
+  );
+
   return (
     <StepsFeatures
       {...props}
+      callToAction={mdCurrentForm}
       step={props.cloudPhase}
     >
       {[{
         title: "Connect to Azure",
         description:
           "Bring your own Azure Cloud connection or get started with a Fathym Managed Azure Subscription.",
-        children: <CloudConnectForms class="px-4" />,
+        children: smCurrentForm,
       }, {
         title: "Cloud Landing Zone",
         description:
           "Deploy Fathym's Composable Application Landing Zone (CALZ) to prepare your cloud for devices and applications.",
-        children: <CloudCALZForm class="px-4" />,
+        children: smCurrentForm,
       }, {
         title: "IoT Infrastructure",
         description:
           "Establish the foundation of your IoT infrastructure for the creation of device flows and data access.",
-        children: <CloudIoTForm class="px-4" />,
+        children: smCurrentForm,
       }]}
     </StepsFeatures>
   );
