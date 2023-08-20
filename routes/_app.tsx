604a343fbc61f3269375b1f14bb7f80acc11cbf3
mcgear <michael.gearhardt@gmail.com>
2023-08-20 09:27:08 -0600

    unused imports

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,21 +1,9 @@
 import { Head } from "$fresh/runtime.ts";
-import {
-  Action,
-  BasicLayout,
-  classSet,
-  factory,
-  Footer,
-  Header,
-  HeaderLogo,
-} from "@fathym/atomic";
+import { BasicLayout } from "@fathym/atomic";
 
 import { AppProps } from "$fresh/server.ts";
 import BiotechHeader from "../components/organisms/BiotechHeader.tsx";
 import BiotechFooter from "../components/organisms/BiotechFooter.tsx";
-import InteractiveResponsiveSet from "../islands/molecules/InteractiveResponsiveSet.tsx";
-import ProfileMenu from "../islands/common/ProfileMenu.tsx";
-import { ChevronDownIcon } from "../build/iconset/icons/ChevronDownIcon.tsx";
-import { UserIcon } from "../build/iconset/icons/UserIcon.tsx";
 
 export default function App({ Component }: AppProps) {
   const header = <BiotechHeader />;
