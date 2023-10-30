3ef3765d86ced3dbedaa99d45f989c150145475e
mcgear <michael.gearhardt@gmail.com>
2023-10-30 11:53:22 -0600

    Move config file

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from '$fresh/server.ts';
-import twindPlugin from '$fresh/plugins/twind.ts';
-import twindConfig from './twind.config.ts';
-import { iconSetPlugin } from '@fathym/atomic-icons';
-import { curIconSetGenerateConfig } from './fathym-atomic-icons.config.ts';
+import { defineConfig } from "$fresh/server.ts";
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+import { iconSetPlugin } from "@fathym/atomic-icons";
+import { curIconSetGenerateConfig } from "./configs/fathym-atomic-icons.config.ts";
 
 export default defineConfig({
   plugins: [
