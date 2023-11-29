9a778f0c370be26b2a5dbbe73a07cfeb04d836df
mcgear <michael.gearhardt@gmail.com>
2023-11-29 14:22:38 -0700

    Handle bypassing sign in for data apis

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -1,47 +1,51 @@
-import { getCookies, setCookie } from "$std/http/cookie.ts";
-import { cookieSession, redisSession } from "$fresh/session";
-import { MiddlewareHandlerContext } from "$fresh/server.ts";
-import { createGitHubOAuthConfig, createHelpers } from "$fresh/oauth";
-import { connect } from "redis";
-import { OpenBiotechManagerState } from "../src/OpenBiotechManagerState.tsx";
-import { SetupPhaseTypes } from "../src/SetupPhaseTypes.tsx";
-import { CloudPhaseTypes } from "../src/CloudPhaseTypes.tsx";
-import { redirectRequest } from "@fathym/common";
-import { OpenBiotechEaC } from "../src/eac/OpenBiotechEaC.ts";
-import { denoKv } from "../configs/deno-kv.config.ts";
-import { eacSvc } from "../services/eac.ts";
-import { DevicesPhaseTypes } from "../src/DevicesPhaseTypes.tsx";
-import { jwtConfig } from "../configs/jwt.config.ts";
+import { getCookies, setCookie } from '$std/http/cookie.ts';
+import { cookieSession, redisSession } from '$fresh/session';
+import { MiddlewareHandlerContext } from '$fresh/server.ts';
+import { createGitHubOAuthConfig, createHelpers } from '$fresh/oauth';
+import { connect } from 'redis';
+import { OpenBiotechManagerState } from '../src/OpenBiotechManagerState.tsx';
+import { SetupPhaseTypes } from '../src/SetupPhaseTypes.tsx';
+import { CloudPhaseTypes } from '../src/CloudPhaseTypes.tsx';
+import { redirectRequest } from '@fathym/common';
+import { OpenBiotechEaC } from '../src/eac/OpenBiotechEaC.ts';
+import { denoKv } from '../configs/deno-kv.config.ts';
+import { eacSvc } from '../services/eac.ts';
+import { DevicesPhaseTypes } from '../src/DevicesPhaseTypes.tsx';
+import { jwtConfig } from '../configs/jwt.config.ts';
 
 const { signIn, handleCallback, signOut, getSessionId } = createHelpers(
   createGitHubOAuthConfig({
-    scope: ["user:email"],
-  }),
+    scope: ['user:email'],
+  })
 );
 
 async function loggedInCheck(
   req: Request,
-  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>,
+  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>
 ) {
   const url = new URL(req.url);
 
   const { pathname } = url;
 
+  if (pathname.startsWith('/api/eac/data/')) {
+    return ctx.next();
+  }
+
   switch (pathname) {
-    case "/signin": {
+    case '/signin': {
       return await signIn(req);
     }
 
-    case "/signin/callback": {
+    case '/signin/callback': {
       const { response, tokens, sessionId } = await handleCallback(req);
 
       const { accessToken } = tokens;
 
       const resp = await fetch(`https://api.github.com/user/emails`, {
         headers: {
-          Accept: "application/vnd.github+json",
+          Accept: 'application/vnd.github+json',
           Authorization: `Bearer ${accessToken}`,
-          "X-GitHub-Api-Version": "2022-11-28",
+          'X-GitHub-Api-Version': '2022-11-28',
         },
       });
 
@@ -52,18 +56,18 @@ async function loggedInCheck(
       const oldSessionId = await getSessionId(req);
 
       if (oldSessionId) {
-        await denoKv.delete(["User", "Session", oldSessionId!, "Username"]);
+        await denoKv.delete(['User', 'Session', oldSessionId!, 'Username']);
       }
 
       await denoKv.set(
-        ["User", "Session", sessionId!, "Username"],
-        primaryEmail!.email,
+        ['User', 'Session', sessionId!, 'Username'],
+        primaryEmail!.email
       );
 
       return response;
     }
 
-    case "/signout": {
+    case '/signout': {
       return await signOut(req);
     }
 
@@ -74,10 +78,10 @@ async function loggedInCheck(
         return redirectRequest(`/signin?success_url=${pathname}`);
       } else {
         const currentUsername = await denoKv.get<string>([
-          "User",
-          "Session",
+          'User',
+          'Session',
           sessionId,
-          "Username",
+          'Username',
         ]);
 
         if (currentUsername.value) {
@@ -94,13 +98,13 @@ async function loggedInCheck(
 
 async function currentEaC(
   req: Request,
-  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>,
+  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>
 ) {
   const currentEaC = await denoKv.get<string>([
-    "User",
+    'User',
     ctx.state.Username,
-    "Current",
-    "EaC",
+    'Current',
+    'EaC',
   ]);
 
   let eac: OpenBiotechEaC | undefined = undefined;
@@ -125,9 +129,9 @@ async function currentEaC(
 
 async function currentState(
   req: Request,
-  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>,
+  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>
 ) {
-  const isAuthenticated = ctx.state.session.get("isMsalAuthenticated");
+  const isAuthenticated = ctx.state.session.get('isMsalAuthenticated');
 
   // Call to get state
   const state: OpenBiotechManagerState = {
@@ -138,7 +142,7 @@ async function currentState(
       Phase: CloudPhaseTypes.Connect,
     },
     Devices: {
-      JWT: "",
+      JWT: '',
       Phase: DevicesPhaseTypes.Connect,
     },
   };
@@ -163,7 +167,7 @@ async function currentState(
 
         const iotResGroup = resGroups[state.Cloud.ResourceGroupLookup!];
 
-        if ("iot-flow" in (iotResGroup.Resources || {})) {
+        if ('iot-flow' in (iotResGroup.Resources || {})) {
           // TODO: Re-enable
           // state.Cloud.Phase = CloudPhaseTypes.Complete;
 
@@ -174,8 +178,8 @@ async function currentState(
           if (iots.length > 0) {
             state.Devices.IoTLookup = iots[0];
 
-            const iotFlowResource = iotResGroup
-              .Resources![state.Devices.IoTLookup]!;
+            const iotFlowResource =
+              iotResGroup.Resources![state.Devices.IoTLookup]!;
 
             state.Devices.Phase = DevicesPhaseTypes.Connect;
 
@@ -192,11 +196,11 @@ async function currentState(
               const username = ctx.state.Username;
 
               const currentJwt = await denoKv.get<string>([
-                "User",
+                'User',
                 username,
-                "EaC",
+                'EaC',
                 entLookup,
-                "JWT",
+                'JWT',
               ]);
 
               if (currentJwt.value) {
@@ -216,7 +220,7 @@ async function currentState(
               }
             }
           } else {
-            state.Devices.IoTLookup = "iot-flow";
+            state.Devices.IoTLookup = 'iot-flow';
           }
         }
       }
@@ -232,7 +236,7 @@ const session = cookieSession();
 
 function userSession(
   req: Request,
-  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>,
+  ctx: MiddlewareHandlerContext<OpenBiotechManagerState>
 ) {
   return session(req, ctx);
 }
