258e99d7130782fe6033b21df471c83250b2fe36
mcgear <michael.gearhardt@gmail.com>
2024-01-12 14:13:06 -0700

    No biotech dashboard

--- routes/index.tsx
+++ routes/index.tsx
@@ -68,7 +68,7 @@ export default function Home({
 
       {initialSteps}
 
-      {state.Phase > 1 ? <BiotechDashboard /> : <></>}
+      {/* {state.Phase > 1 ? <BiotechDashboard /> : <></>} */}
     </>
   );
 }
