cb2647a0a932c87ba3e5748bb7ef77bd2ff51fda
Michael Gearhardt <michael.gearhardt@gmail.com>
2024-01-14 01:09:24 -0700

    Update oAuth.config.ts

--- configs/oAuth.config.ts
+++ configs/oAuth.config.ts
@@ -1,1 +1,20 @@
 
+import { createAzureOAuth, createGitHubOAuth } from "@fathym/eac";
+
+export const gitHubOAuth = createGitHubOAuth(["admin:org", "user:email"]);
+
+export const azureOBiotechOAuth = createAzureOAuth(
+  "AZURE_O_BIOTECH",
+  "http://localhost:8000/signin/callback",
+  [
+    "openid",
+  ],
+);
+
+export const azureOAuth = createAzureOAuth(
+  "MSAL",
+  "http://localhost:8000/signin/callback",
+  [
+    "https://management.core.windows.net//.default",
+  ],
+);
