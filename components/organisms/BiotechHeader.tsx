97865fac221b454ab9ecca21b075684f23de6804
mcgear <michael.gearhardt@gmail.com>
2024-02-22 11:10:44 -0700

    Develop to API

--- components/organisms/BiotechHeader.tsx
+++ components/organisms/BiotechHeader.tsx
@@ -70,7 +70,7 @@ export function BiotechHeader(props: BiotechHeaderProps) {
                     : "",
                 ])}
               >
-                Develop
+                API
               </Action>
             )}
 
