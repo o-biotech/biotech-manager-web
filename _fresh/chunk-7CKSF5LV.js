244ccb822942a97595ea66d0645bec1e70629c86
mcgear <michael.gearhardt@gmail.com>
2024-03-05 10:20:59 -0700

    delete fresh folder

--- _fresh/chunk-7CKSF5LV.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunk-F4TDH6VI.js";var r=o("span",{class:"-:bg-gradient-to-r -:from-blue-500 -:to-purple-500/75 -:hover:bg-gradient-to-r -:hover:from-purple-500 -:hover:to-blue-500/75"});export{r as a};
